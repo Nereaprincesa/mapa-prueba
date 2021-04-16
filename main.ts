55b9f17821a6f31a358048a6621243be5e3df4cc
Nereaprincesa <81380410+Nereaprincesa@users.noreply.github.com>
2021-04-16 18:58:52 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,36 @@
+scene.onHitWall(SpriteKind.Player, function (sprite, location) {
+	
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.startEffect(effects.spray, 1000)
+})
+info.onLifeZero(function () {
+    game.over(true, effects.confetti)
+})
+let mySprite: Sprite = null
+mySprite = sprites.create(img`
+    . . . . . f f 4 4 f f . . . . . 
+    . . . . f 5 4 5 5 4 5 f . . . . 
+    . . . f e 4 5 5 5 5 4 e f . . . 
+    . . f b 3 e 4 4 4 4 e 3 b f . . 
+    . . f 3 3 3 3 3 3 3 3 3 3 f . . 
+    . f 3 3 e b 3 e e 3 b e 3 3 f . 
+    . f 3 3 f f e e e e f f 3 3 f . 
+    . f b b f b f e e f b f b b f . 
+    . f b b e 1 f 4 4 f 1 e b b f . 
+    f f b b f 4 4 4 4 4 4 f b b f f 
+    f b b f f f e e e e f f f b b f 
+    . f e e f b d d d d b f e e f . 
+    . . e 4 c d d d d d d c 4 e . . 
+    . . e f b d b d b d b b f e . . 
+    . . . f f 1 d 1 d 1 d f f . . . 
+    . . . . . f f b b f f . . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite, 100, 100)
+tiles.setTilemap(tilemap`level3`)
+scene.cameraFollowSprite(mySprite)
+info.setScore(0)
+info.changeScoreBy(1)
+info.setLife(3)
+mySprite.say("hola soy Martina, recoge todas gemas ", 5000)
+music.playMelody("C5 B A G F E D C ", 120)
